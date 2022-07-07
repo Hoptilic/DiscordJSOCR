cc3dbeb037c791d34c472406644bc148a4b429af
Hoptilic <hoptilic@yahoo.com>
2022-07-07 13:45:01 +0200

    Start of async
    Currently broken - Do not use

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 require("dotenv").config()
 const { Client, Intents } = require('discord.js');
 const Tesseract = require("tesseract.js"); // Initializing Tesseract, this module is essential for OCR
+const { createWorker, createScheduler } = require('tesseract.js');
 const scheduler = createScheduler();
 const worker1 = createWorker();
 const worker2 = createWorker();
@@ -34,17 +35,16 @@ bot.on("message", (msg) => {
         console.log(getExt(attachment.proxyURL));
         var ImageURL = attachment.proxyURL;
         var start = Date.now();
-        Tesseract.recognize(
-          ImageURL,
-          "eng",
-          { logger: (m) => console.log(m) }
-        ).then(({ data: { text } }) => {
 
+        function RecogniseStuff(imageurl){
+          Tesseract.recognize(imageurl, "eng", {logger: (m) => console.log(m)}).then(({ data: { text } }) => { return text });
+        };
         var end = Date.now();
         var totaltime = Math.floor((end-start) / 1000);
         console.log(totaltime);
         function checkProfanity(input){
-          let textInput = input.split(" ");
+          console.log(input);
+          let textInput = input.toString().split(" ");
           var checkNWord = textInput.some(e => regexn.test(e));
           var checkFWord = textInput.some(el => regexf.test(el));
           if (checkNWord || checkFWord == true){
@@ -53,30 +53,28 @@ bot.on("message", (msg) => {
             return false;
           }
         }
-          if (checkProfanity(text) == true) {
-              const exampleEmbed = new MessageEmbed()
-              .setColor('#0099ff')
-              .setTitle('Image Filtering')
-              .addFields(
-                { name: 'Flagged Content:', value: attachment.proxyURL },
-                { name: 'User:', value: '<' + '@' + msg.author.id + '>', inline: true},
-                { name: 'ID:', value: msg.author.id, inline: true},
-                { name: 'ㅤ', value: 'ㅤ', inline: false},
-                { name: 'Profanity Found?', value: 'Profanity Present: ' + checkProfanity(text), inline: true},
-                { name: 'Processing Time:', value: totaltime + ' Seconds', inline: true},
-                { name: 'ㅤ', value: 'ㅤ', inline: false},
-                { name: 'Image Extension:', value: '.' + getExt(attachment.proxyURL), inline: true},
-                { name: 'Sent From:', value: '<' + '#' + sentChannel + '>', inline: true},
-              )
-              .setImage(attachment.proxyURL)
-              .setTimestamp()
-              console.log(text);
-              bot.channels.cache.get(reportchannel).send({ embeds: [exampleEmbed] });  
+        let textresult = RecogniseStuff(ImageURL);
+        if (checkProfanity(textresult) == true) {
+            const exampleEmbed = new MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle('Image Filtering')
+            .addFields(
+              { name: 'Flagged Content:', value: attachment.proxyURL },
+              { name: 'User:', value: '<' + '@' + msg.author.id + '>', inline: true},
+              { name: 'ID:', value: msg.author.id, inline: true},
+              { name: 'ㅤ', value: 'ㅤ', inline: false},
+              { name: 'Profanity Found?', value: 'Profanity Present: ' + checkProfanity(text), inline: true},
+              { name: 'Processing Time:', value: totaltime + ' Seconds', inline: true},
+              { name: 'ㅤ', value: 'ㅤ', inline: false},
+              { name: 'Image Extension:', value: '.' + getExt(attachment.proxyURL), inline: true},
+              { name: 'Sent From:', value: '<' + '#' + sentChannel + '>', inline: true},
+            )
+            .setImage(attachment.proxyURL)
+            .setTimestamp()
+            bot.channels.cache.get(reportchannel).send({ embeds: [exampleEmbed] });  
 
-          }
-          console.log(text);
-          msg.delete();
-        });
+        }
+        msg.delete();
       };
     });
   };
