77ebf5174e3366124869f997e3b43d77dac98f15
Hoptilic <hoptilic@yahoo.com>
2022-07-08 11:12:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ let start = 1;
 let end = 2;
 let totaltime = 0;
 let profanity = '';
-var regexn = /([n|N|m|M|j|J|\s]+[|i|I|1|L|l|!|\s]+[|g|G|\s])\w+/;
+var regexn = /([n|N|m|M|j|J]+[|i|I|1|L|l|!]+[|g|G])+/;
 var regexf = /(f|F)(a|A|4|@)(g|G)\w+/;
 var reportchannel = '994648347308732436';
 var sentChannel = '';
@@ -65,8 +65,9 @@ bot.on("message", (msg) => {
         };
         function checkProfanity(input){
           try{
-            let trimmed = input.replace(/\s+/g, '');
+            let trimmed = input.replace(/[\s|.]+/g, '');
             console.log(input);
+            console.log(trimmed);
             let textInput = trimmed.toString().split(" ");
             var checkNWord = textInput.some(e => regexn.test(e));
             var checkFWord = textInput.some(el => regexf.test(el));
